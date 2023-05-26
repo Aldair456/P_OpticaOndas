841886fb7ad74b63157f39f4677d76508554fb9f
aldair.seminario@utec.edu.pe <aldair.seminario@utec.edu.pe>
2023-05-25 22:41:03 -0500

    Nuevos cambios

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import  numpy as np
+from tabulate import tabulate  # Importar la biblioteca tabulate
+from scipy.optimize import curve_fit
 # Exportar de archivo txt
 def read_file(nombre_archivo):
     data = []
@@ -10,25 +12,16 @@ def read_file(nombre_archivo):
                 data.append(datos_linea)
     except FileNotFoundError:
         print("El archivo no se encontró.")
-
     return data
-
 x_masa = []  # lista de la masa
 y_frecuencia = []
 data = read_file('Data_oscilador.txt')  # Leer los datos del archivo una vez
+headers = ["Masa (kg)", "Frecuencia (Hz)"]
+print(tabulate(data, headers=headers, tablefmt="grid"))
 
 for i in range(1, 10):  # Recorrer los datos desde el índice 1 al 9
     y_frecuencia.append(float(data[i][0]))  # Convertir a float
     x_masa.append(float(data[i][1]))  # Convertir a float
-
-# Imprimir los datos para verificar
-print("MOSTRANDO FRECUENCIA")
-for i in range(len(y_frecuencia)):
-    print(y_frecuencia[i])
-
-print("MOSTRANDO LA MASA")
-for i in range(len(x_masa)):
-    print(x_masa[i])
 
 # Configurar el estilo del gráfico
 plt.style.use('seaborn-darkgrid')
@@ -44,51 +37,61 @@ ax.plot(x_masa, y_frecuencia, 'o-', color='k', linewidth=2, markersize=8)
 
 # Configurar etiquetas y título
 ax.set_xlabel('Masa (kg)')
-ax.set_ylabel('Frecuencia')
+ax.set_ylabel('Frecuencia (Hz)')
 ax.set_title('Frecuencia de oscilación vs Masa del oscilador armónico')
 
 # Configurar límites de los ejes
-ax.set_xlim(min(x_masa)-10, max(x_masa)+10)
-ax.set_ylim(min(y_frecuencia)-0.5, max(y_frecuencia)+0.5)
+ax.set_xlim(min(x_masa)-0.1, max(x_masa)+0.1)
+ax.set_ylim(min(y_frecuencia)-0.1, max(y_frecuencia)+0.1)
 
 # Configurar leyenda
 ax.legend(['Datos'], loc='best')
-
 # Mostrar y guardar el gráfico
 plt.tight_layout()
 plt.show()
-fig.savefig('grafico2.png')
+fig.savefig('grafico_original.png')
+# todo realizando el cambio
+def f(m, k):
 
+    return k / (2 * np.pi) * np.sqrt(1 / m)
 
-#-----
+# Leer los datos del archivo
+data = np.genfromtxt('Data_oscilador.txt', delimiter=',', skip_header=1)
+
+# Obtener los valores de masa y frecuencia
+x_masa = data[:, 1]
+y_frecuencia = data[:, 0]
+
 # Realizar el ajuste de curva
-grado_ajuste = 2  # Grado del polinomio de ajuste (puedes ajustarlo según tus necesidades)
-ajuste = np.polyfit(x_masa, y_frecuencia, grado_ajuste)
-parametros_ajuste = ajuste.tolist()
-tipo_ajuste = f"Polinomio de grado {grado_ajuste}"
-# Imprimir los parámetros del ajuste y el tipo de ajuste
-print("Parámetros del ajuste:", parametros_ajuste)
-print("Tipo de ajuste utilizado:", tipo_ajuste)
+popt, _ = curve_fit(f, x_masa, y_frecuencia)
+
+# Obtener el valor del parámetro k
+k = popt[0]
+
+# Generar valores de masa para graficar la función ajustada
+x_masa_fit = np.linspace(min(x_masa), max(x_masa), 100)
+
+# Calcular los valores de frecuencia ajustados
+y_frecuencia_fit = f(x_masa_fit, k)
+
+# Configurar el estilo del gráfico
+plt.style.use('seaborn-darkgrid')
+
+# Configurar el tamaño de fuente
+plt.rcParams.update({'font.size': 12})
 
 # Crear el gráfico
 fig, ax = plt.subplots(figsize=(8, 6))
-
 # Graficar los datos
-ax.plot(x_masa, y_frecuencia, 'o-', color='k', linewidth=2, markersize=8, label='Datos')
+ax.plot(x_masa, y_frecuencia, 'o', color='k', label='Datos')
 
-# Añadir la curva de ajuste al gráfico
-ajuste_x = np.linspace(min(x_masa), max(x_masa), 100)
-ajuste_y = np.polyval(ajuste, ajuste_x)
-ax.plot(ajuste_x, ajuste_y, '--', color='r', linewidth=2, label='Ajuste')
+# Graficar la función ajustada
+ax.plot(x_masa_fit, y_frecuencia_fit, '--', color='r', label='Ajuste')
 
 # Configurar etiquetas y título
 ax.set_xlabel('Masa (kg)')
-ax.set_ylabel('Frecuencia')
+ax.set_ylabel('Frecuencia (HZ)')
 ax.set_title('Frecuencia de oscilación vs Masa del oscilador armónico')
-
-# Configurar límites de los ejes
-ax.set_xlim(min(x_masa)-10, max(x_masa)+10)
-ax.set_ylim(min(y_frecuencia)-0.5, max(y_frecuencia)+0.5)
 
 # Configurar leyenda
 ax.legend(loc='best')
@@ -96,8 +99,10 @@ ax.legend(loc='best')
 # Mostrar el gráfico
 plt.tight_layout()
 plt.show()
-
-# Guardar el gráfico en formato png con un nombre descriptivo
-nombre_grafico = 'ajuste_pol_grado2.png'
-fig.savefig(nombre_grafico)
-print("Gráfico guardado como", nombre_grafico)
+fig.savefig('grafico_conajuste.png')
+# Imprimir los parámetros del ajuste
+# Imprimir los parámetros del ajuste y el tipo de ajuste utilizado
+print("Parámetros del ajuste:")
+for i, param in enumerate(popt):
+    print(f"Parámetro  K  = {i+1}: {param}")
+print("Tipo de ajuste utilizado: funcion racional   ")
